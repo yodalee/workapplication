c4708b54148f1f1970bd95358c7b20769a157d5c
yodalee <lc85301@gmail.com>
2015-03-29 10:59:15 +0800

    add code skeleton and makefile

---
+++ orienteering.cpp
@@ -0,0 +1,14 @@
+class Orienteering {
+public:
+  void main();
+};
+
+void Orienteering::main() {
+  // TODO: Implement this function
+}
+
+int main(int argc, char* argv[]) {
+  Orienteering o;
+  o.main();
+  return 0;
+}
