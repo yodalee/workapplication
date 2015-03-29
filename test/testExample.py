a2d2bcc0fffafc7efa97138c31c5b64f16c05311
yodalee <lc85301@gmail.com>
2015-03-29 11:53:44 +0800

    add example test case and python unittest

---
+++ test/testExample.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import unittest
+import subprocess
+
+runCmd = "./../a.out"
+testDir = "testcase"
+testlist = ["example1", "example2", "example3", "invalid"]
+anslist = [4, 9, 6, -1]
+
+
+class TestOrienteering(unittest.TestCase):
+    def setUp(self):
+        pass
+
+
+def execute(test):
+    testin = open("%s/%s" % (testDir, test))
+    p = subprocess.Popen(
+        runCmd.split(), stdin=testin, stdout=subprocess.PIPE)
+    testin.close()
+    return p.stdout.read().decode('utf-8')
+
+
+def create_test(test, ans):
+    def do_test(self):
+        self.assertEqual(execute(test), ans)
+    return do_test
+
+if __name__ == '__main__':
+    for test, ans in zip(testlist, anslist):
+        test_method = create_test(test, ans)
+        test_method.__name__ = "test_%s" % (test)
+        setattr(TestOrienteering, test_method.__name__, test_method)
+
+    unittest.main()
