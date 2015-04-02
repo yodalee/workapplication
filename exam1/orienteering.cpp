08bbf2f535154091394e8dc8659da922dc8dd382
yodalee <lc85301@gmail.com>
2015-04-02 23:45:28 +0800

    call buildGraph in main, invalid test passed

--- exam1/orienteering.cpp
+++ exam1/orienteering.cpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+#include <climits>
 
 #include "maze.h"
 
@@ -12,7 +14,18 @@ void Orienteering::main() {
   maze = new Maze();
   if(maze->parse()) {
     cout << -1;
+    return;
   }
+  vector<vector<int> >vec;
+  maze->buildGraph(vec);
+
+  for (auto row : vec) {
+    if (find(row.begin(), row.end(), INT_MAX) != row.end()) {
+      cout << -1;
+      return;
+    }
+  }
+
 }
 
 int main(int argc, char* argv[]) {
