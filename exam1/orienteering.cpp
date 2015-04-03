a35d86b9f935a32cc93c361ea6da633a85d95770
yodalee <lc85301@gmail.com>
2015-04-03 10:46:50 +0800

    tsp function, add dummy point into distance table

--- exam1/orienteering.cpp
+++ exam1/orienteering.cpp
@@ -1,14 +1,32 @@
 #include <algorithm>
 #include <climits>
+#include <vector>
 
 #include "maze.h"
 
 class Orienteering {
 public:
   void main();
+  int solveTSP(vector<vector<int> > &);
 private:
   Maze *maze;
+  int numPoints;
 };
+
+int
+Orienteering::solveTSP(vector<vector<int> > &disTable)
+{
+  //add dummy node only connect to end node, force TSP stop there
+  disTable.push_back(vector<int>(numPoints-1, -1));
+  for (int i = 0; i < numPoints; ++i) {
+    disTable[i].push_back(-1);
+    disTable[i][i] = -1;
+  }
+  disTable[numPoints-2][numPoints-1] = 0;
+  disTable[numPoints-1][numPoints-2] = 0;
+
+  return 0;
+}
 
 void Orienteering::main() {
   maze = new Maze();
@@ -19,6 +37,7 @@ void Orienteering::main() {
   vector<vector<int> >vec;
   maze->buildGraph(vec);
 
+  //search unreachable checkpoints, print -1 if there is unreachable
   for (auto row : vec) {
     if (find(row.begin(), row.end(), INT_MAX) != row.end()) {
       cout << -1;
@@ -26,6 +45,8 @@ void Orienteering::main() {
     }
   }
 
+  numPoints = vec.size() + 1;
+  cout << solveTSP(vec);
 }
 
 int main(int argc, char* argv[]) {
